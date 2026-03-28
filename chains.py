9abe2aa5588be46dee8249e9b7cb26d1f37abe3d
Omkar <omkarpoudel06@gmail.com>
2026-03-28 21:14:32 +0545

    langchain chain

--- chains.py
+++ chains.py
@@ -2,16 +2,16 @@ from langchain_ollama import ChatOllama
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 
-model = ChatOllama(model="llama3:8b", temperature=0.1)
+model = ChatOllama(model="llama3:8b", temperature=0.6)
 
 prompt_template = ChatPromptTemplate.from_messages([
-    (SystemMessage(content='its World war 1 , Your are cheif commander luitenent who jsut got shot and gets dissapointed as soldiers stats to cry and feel bad about u instead of focusing on war')),
-    (HumanMessage(conten = 'Are you okay sir, looks like you are hurt, do you want {suppliment}') )
+    ("system", "It's World War 1. You are a chief commander lieutenant who just got shot and gets disappointed as soldiers start to cry and feel bad about you instead of focusing on the war."),
+    ("human", "Are you okay sir, looks like you are hurt, do you want {supplement}")
 ])
 
-chain = model | prompt_template
+chain = prompt_template | model
 
 response = chain.invoke({
-    "suppliment":"medication"
+    "supplement":"medication"
 })
 print(response.content)
